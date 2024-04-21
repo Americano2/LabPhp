d6cd33a1faf3ebb061ae9641da8fd04d3cf9fd3f
Klishch Viktor <138811982+Americano2@users.noreply.github.com>
2024-04-21 21:27:43 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<html >
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"></link>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+		<title>Тest page</title>
+	</head>
+	<body>
+		<form id="form1" name="form1" method="get" action="php.php">
+			<label class="my-2">Значення x=
+				<input type="text" name="x" />
+			</label>
+			<p>
+			<label class="my-2">Значення y=
+			<input type="text" name="y" />
+			</label>
+			</p>
+			<p>
+			<input class="btn btn-primary" name="submit" type="submit" id="submit" lang="uk" value="Submit" />
+			</p>
+		</form>
+	</body>
+</html>
